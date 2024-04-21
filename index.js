18f0086b8ae8ca8777573848d2ed49fcc535c161
Naman Arora <aroranaman17@outlook.com>
2024-04-21 20:26:57 +1000

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ var Typer = {
         $.get(Typer.file, function (data) {
             Typer.text = data;
             Typer.text = Typer.text.slice(0, Typer.text.length - 1);
-            Typer.write(Typer.text);
-            clearInterval(accessCountimer);
         });
     },
 
